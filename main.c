adc9192af182249648cc6b239a07a98b1257a09b
Farimah Nourpanah <139590346+Farimah84@users.noreply.github.com>
2024-04-13 12:31:30 +0330

    Update main.c

--- main.c
+++ main.c
@@ -1,9 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int main()
+{
+    int decimal;
+    printf("Enter a decimal number: ");
+    scanf("%d", &decimal);
+
+    // Call function to convert decimal to binary
+    decimalToBinary(decimal);
+
+    return 0;
+}
+
+
 void decimalToBinary(int decimal)
 {
-    // Array to store binary number
     int binary[32];
 
     // Counter for binary array
@@ -17,23 +29,10 @@ void decimalToBinary(int decimal)
     }
 
     // Print binary array in reverse order
-    printf("Binary equivalent: ");
-    for (int j = i - 1; j >= 0; j--) {
+    printf("Binary: ");
+    for (int j = i - 1; j >= 0; j--)
+    {
         printf("%d", binary[j]);
     }
     printf("\n");
-}
-
-int main()
-{
-    int decimal;
-
-    // Input decimal number
-    printf("Enter a decimal number: ");
-    scanf("%d", &decimal);
-
-    // Call function to convert decimal to binary
-    decimalToBinary(decimal);
-
-    return 0;
 }
